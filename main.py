8cebfbaf5c5d07a5be2a178b6fa6626bb73bb077
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-05-31 14:29:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,10 @@
 import numpy as np
 import streamlit as st
 st.title("Welcome to My Home")
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
+
+
+
