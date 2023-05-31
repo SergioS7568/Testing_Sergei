b84825cc644b1a41e16e3ae126223cbde919610c
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-05-31 15:00:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
 
 
-from keras.models import load_model
-new_model = tf.keras.models.load_model('saved_model/mdl_ws.hdf5')
+
+loaded_model = tf.keras.models.load_model('saved_model/mdl_ws.hdf5')
 ### load file
 uploaded_file = st.file_uploader("Choose a image file", type="jpg")
 
