62dda5f5f4fbeecf98e3007fb4613cf6400e65cd
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-05-31 15:47:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -142,11 +142,9 @@ if uploaded_file is not None:
     resized = mobilenet_v2_preprocess_input(resized)
     img_reshape = resized[np.newaxis,...]
     
-
-    def prepare(filepath):
+    img_array = cv2.imdecode(file_bytes, 1)
     new_array = cv2.resize(opencv_image, (28, 28))
     arr=new_array.reshape(-1, 28, 28, 3)
-    return arr
 
     x=prepare('img.jpg')
     x = tf.keras.utils.normalize(x, axis=1)
