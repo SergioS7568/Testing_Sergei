d976257be3dc1553b9c380649f4eb539ca216b59
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-05-31 15:10:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ if uploaded_file is not None:
 
     Genrate_pred = st.button("Generate Prediction")
     if Genrate_pred:
-       prediction = model.predict(img_reshape).argmax()
+       prediction = loaded_model.predict(img_reshape).argmax()
        st.title("Predicted Label for the image is {}".format(map_dict [prediction]))
 
 
