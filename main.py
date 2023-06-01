13c1f4e22b06fc30221b3acbb63eb61b753ebaa7
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-05-31 22:41:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -150,9 +150,12 @@ if uploaded_file is not None:
         prediction = loaded_model(img_reshape.reshape(-1, 28, 28, 1))
         #print(prediction)
         #pred_name = np.argmax(prediction, axis=1) 
+        #print(pred_name)
+        #i=pred_name
+        #print(map_dict[i])
         preds_name = np.exp(prediction[:,1])
-        print(pred_name)
-        i=pred_name
+        print(preds_name)
+        i=preds_name
         print(map_dict[i])
         
         #probs = np.exp(map_dict[:,1])      
