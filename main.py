f91ebc029ad895cb7fc0bac1be400bec653bfe03
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-05-31 22:22:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ if uploaded_file is not None:
         #prediction = loaded_model([img_reshape.reshape(-1, 28, 28, 1)])
         #print(prediction[0])
         prediction = loaded_model(img_reshape.reshape(-1, 28, 28, 1))
-        pred_name = CATEGORIES[np.argmax(prediction)]
+        pred_name = map_dict[np.argmax(prediction)]
         st.title("Predicted Label for the image is {}".pred_name)
         #st.image(opencv_image, caption=prediction.shape)
         #st.title("Predicted Label for the image is {}".map_dict(prediction))
