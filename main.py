d6dbd183570713cae80bd9d6dbb83b4025392d29
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-02 17:23:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -39,11 +39,11 @@ if uploaded_file is not None:
         prediction = loaded_model(img_reshape.reshape(-1, 28, 28, 1))
         print("THIS ")
         #prediction = loaded_model(img_reshape).argsort()
-        print(prediction.shape)
+        #print(prediction.shape)
         print(" IS ")
-        #classes = np.argmax(prediction, axis = 1)
-        #print("WORKING  ")
-        #print(classes)
+        classes = np.argmax(prediction, axis = 1)
+        print("WORKING  ")
+        print(classes)
         #print("AS INTENDED ")
         
         
