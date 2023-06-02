1e091ea3705438504593df65e392fcdf46518263
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-02 19:00:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ if uploaded_file is not None:
 
 
     if Genrate_pred:
-        img_reshaped = img_reshape.reshape(null, 28, 28, 3)
+        img_reshaped = img_reshape.reshape(Null, 28, 28, 3)
         prediction = loaded_model.predict(img_reshape).argmax()
         print(prediction)
         #print=  (classes[prediction])
