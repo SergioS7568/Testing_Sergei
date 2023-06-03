0b8dd7bdd94fe4dfae8f53c3f517393845ce1017
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-03 17:47:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -167,11 +167,12 @@ if uploaded_file is not None:
         #plt.imshow(img.squeeze()) 
         #plt.imshow(img_reshape) 
         #np.expand_dims(img_reshape, axis=0)
-        img_reshape = img_reshape.reshape(-1, 28, 28, 1)
+        img_reshape = img_reshape.reshape(-1, 28, 28, float32)
         #pred = loaded_model.predict(img_reshape) 
         #pred = tf.nn.tanh(pred, name ='tanh')
         prediction = loaded_model.predict(img_reshape[:1])
         print("prediction shape:", prediction.shape)
+        tensorshow = tf.
         #print(pred)
         #ind = (-pred).argsort()[:5]
         #print(ind)
