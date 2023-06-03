0cf569aaf3af9f53c57671b6f902d13b86977996
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-02 23:46:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ loaded_model = tf.keras.models.load_model('saved_model/mdl_wt.hdf5')
 ### load file
 uploaded_file = st.file_uploader("Choose a image file", type="jpg")
 
+ classes = ['aircraft carrier', 'airplane', 'alarm clock', 'ambulance', 'angel', 'ant', 'anvil', 'apple', 'asparagus', 'axe', 'backpack', 'banana', 'bandage', 'barn', 'baseball', 'baseball bat', 'basket', 'basketball', 'bat', 'beard', 'bed', 'bee', 'belt', 'bench', 'bicycle', 'binoculars', 'bird', 'birthday cake', 'book', 'boomerang', 'bottlecap', 'bowtie', 'bracelet', 'brain', 'bread', 'bridge', 'broccoli', 'broom', 'bucket', 'bulldozer', 'bus', 'butterfly', 'chandelier', 'church', 'clock', 'coffee cup', 'compass', 'computer', 'cookie', 'cooler', 'couch', 'cow', 'crab', 'crocodile', 'dog', 'dragon', 'garden hose', 'giraffe', 'goatee', 'grass', 'guitar', 'hamburger', 'hammer', 'hat', 'headphones', 'hospital', 'hot air balloon', 'hot dog', 'hourglass', 'mailbox', 'map', 'marker', 'microwave', 'monkey', 'owl', 'paintbrush', 'paint can', 'palm tree', 'paper clip', 'parachute', 'parrot', 'passport', 'power outlet', 'purse', 'rabbit', 'raccoon', 'radio', 'remote control', 'rhinoceros', 'rifle', 'river', 'roller coaster', 'rollerskates', 'sailboat', 'sandwich', 'saw', 'saxophone', 'stairs', 'star', 'steak', 'string bean', 'The Eiffel Tower', 'toothpaste', 'tornado', 'tractor', 'traffic light', 'train', 'violin', 'washing machine', 'watermelon', 'waterslide', 'whale', 'wine bottle']
+
 
 map_dict2 = {0: 'dog',
             1: 'horse',
@@ -164,7 +166,7 @@ if uploaded_file is not None:
         #plt.imshow(img.squeeze()) 
         pred = loaded_model.predict(np.expand_dims(img_reshape, axis=0))[0]
         ind = (-pred).argsort()[:5]
-        latex = [class_names[x] for x in ind]
+        latex = [classes[x] for x in ind]
         print(latex)
         
         #prod = np.argmax(prediction, axis=1)
