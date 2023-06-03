73f58fb88bf472ccafe4bae1f215c242e9acefe9
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-03 19:16:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -164,7 +164,7 @@ if uploaded_file is not None:
 
     if Genrate_pred:
             img_reshape = img_reshape.reshape(-1, 28, 28, 1)
-            pred = loaded_model.predict(img_reshape) 
+            pred = loaded_model.predict(img_reshape).argmax() 
             print(pred)
             #pred_class = classes[pred.argmax()]
             #plt.imshow(img_reshape)
