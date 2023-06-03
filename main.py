50b5ae5d496e00610880e6fb02970b68cc0747b7
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-03 00:22:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -168,7 +168,7 @@ if uploaded_file is not None:
         #plt.imshow(img_reshape) 
         #np.expand_dims(img_reshape, axis=0)
         img_reshape = img_reshape.reshape(-1, 28, 28, 1)
-        pred = (loaded_model.predict(img_reshape).argmax())[0]
+        pred = loaded_model.predict(img_reshape).argmax()[0]
         ind = (-pred).argsort()[:5]
         print(ind)
         latex = ind
