425e8dfa87faab000e4cf03ded91de3bf518eabc
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-02 23:04:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -160,9 +160,9 @@ if uploaded_file is not None:
         #img_reshape = img_reshape.reshape(-1, 28, 28, 1)
         #prediction = loaded_model.predict(img_reshape).argmax()
         #print(loaded_model.predict(img_reshape).argmax())
-        prediction = loaded_model.predict_classes(img_reshape).argmax()
-        st.title("Predicted Label for the image is {}".format(map_dict [prediction]))
-        #print(prediction)
+        prediction = loaded_model.predict(img_reshape)
+        #st.title("Predicted Label for the image is {}".format(map_dict [prediction]))
+        print(prediction)
         #pred = np.exp(prediction[:,3])
         #print(pred)
         
