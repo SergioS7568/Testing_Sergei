5ffc86b9e0a737a7485e210fc7ad5089c63b97c2
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-02 23:14:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -159,14 +159,6 @@ import matplotlib.pyplot as plt
 
     if Genrate_pred:
         img_reshape = img_reshape.reshape(3, 28, 28, 1)
-        #prediction = loaded_model.predict(img_reshape).argmax()
-        #print(loaded_model.predict(img_reshape).argmax())
-        #prediction = loaded_model.predict(img_reshape)
-        #prediction = np.argmax(prediction, axis=1)
-        #st.title("Predicted Label for the image is {}".format(map_dict [prediction]))
-        #print(prediction)
-        #pred = np.exp(prediction[:,3])
-        #print(pred)
         %matplotlib inline  
         idx = img_reshape
         img = x_test[idx]
@@ -179,6 +171,14 @@ import matplotlib.pyplot as plt
         #prod = np.argmax(prediction, axis=1)
         print("hello")
         #pred = np.argmax(prediction)
+        #prediction = loaded_model.predict(img_reshape).argmax()
+        #print(loaded_model.predict(img_reshape).argmax())
+        #prediction = loaded_model.predict(img_reshape)
+        #prediction = np.argmax(prediction, axis=1)
+        #st.title("Predicted Label for the image is {}".format(map_dict [prediction]))
+        #print(prediction)
+        #pred = np.exp(prediction[:,3])
+        #print(pred)
 
         
        
