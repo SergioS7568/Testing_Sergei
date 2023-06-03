a2ef38fa681635a8b8e5c2d712d8f12c746ba8fe
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-02 23:44:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ if uploaded_file is not None:
         #img_reshape = img_reshape.reshape(3, 28, 28, 1)
         img = img_reshape
         plt.imshow(img.squeeze()) 
-        pred = model.predict(np.expand_dims(img, axis=0))[0]
+        pred = loaded_model.predict(np.expand_dims(img, axis=0))[0]
         ind = (-pred).argsort()[:5]
         latex = [class_names[x] for x in ind]
         print(latex)
