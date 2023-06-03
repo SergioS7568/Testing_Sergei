f6e1849b2936bb48f4cb3ac52cbb7d6b4129a8bf
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-02 23:49:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -161,7 +161,7 @@ if uploaded_file is not None:
 
 
     if Genrate_pred:
-        #img_reshape = img_reshape.reshape(3, 28, 28, 1)
+        img_reshape = img_reshape.reshape(3, 28, 28, 1)
         #img = img_reshape
         #plt.imshow(img.squeeze()) 
         pred = loaded_model.predict(np.expand_dims(img_reshape, axis=0))[0]
