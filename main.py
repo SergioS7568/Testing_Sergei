60aa13f638d79f9ce48efaaf334f92153a9bfa3e
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-04 16:44:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,25 +11,3 @@ import time
 
 
 st.title('Green stem Classifier')
-file_uploaded = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
-class_btn = st.button("Classify")
-if file_uploaded is not None:    
-    image = Image.open(file_uploaded)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
-        
-    if class_btn:
-        if file_uploaded is None:
-            st.write("Invalid command, please upload an image")
-        else:
-            with st.spinner('Model working....'):
-                # plt.imshow(image)
-                # plt.axis("off")
-                predictions = predict(image)
-                time.sleep(1)
-                st.success('Classified')
-                st.write(predictions)
-                
-                
-                
-if __name__ == "__main__":
-    main()
