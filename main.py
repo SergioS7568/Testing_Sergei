774e5897ba1b3dc1e26b10e143304a013f5e1cd0
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-04 17:00:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 
+def predictor(image):
+    classifier_model = "keras.h5"
+    loaded_model = tf.keras.models.load_model('saved_model/keras.h5')
+    model = load_model(classifier_model)
+    test_image = image.resize((200,200))
+    test_image = preprocessing.image.img_to_array(test_image)
+    test_image = test_image / 255.0
+    test_image = np.expand_dims(test_image, axis=0)
+    class_names = {0 : 'healthy', 1 :'diseased'}
+    predictions = model.predict(test_image)
+    scores = tf.nn.softmax(predictions[0])
+    scores = scores.numpy()
+    result = f"{class_names[np.argmax(scores)]} with a { (100 * np.max(scores)).round(2) } % confidence." 
+    return result
+
 
 st.title('Green stem Classifier')
 file_uploaded = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
@@ -24,18 +39,5 @@ if file_uploaded is not None:
                 st.success('Classified')
                 st.write(predictions)
                 
-def predictor(image):
-    classifier_model = "keras.h5"
-    loaded_model = tf.keras.models.load_model('saved_model/keras.h5')
-    model = load_model(classifier_model)
-    test_image = image.resize((200,200))
-    test_image = preprocessing.image.img_to_array(test_image)
-    test_image = test_image / 255.0
-    test_image = np.expand_dims(test_image, axis=0)
-    class_names = {0 : 'healthy', 1 :'diseased'}
-    predictions = model.predict(test_image)
-    scores = tf.nn.softmax(predictions[0])
-    scores = scores.numpy()
-    result = f"{class_names[np.argmax(scores)]} with a { (100 * np.max(scores)).round(2) } % confidence." 
-    return result
+
                 
