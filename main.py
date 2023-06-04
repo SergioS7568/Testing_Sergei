728f79aae53d94b2c578680e50178f2e649b1b7e
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-04 17:18:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,31 +18,24 @@ def predictor(image):
     model = loaded_model
     test_image = image.resize(-1,(28,28),1)
     test_image = preprocessing.image.img_to_array(test_image)
-    test_image = test_image / 255.0
     test_image = np.expand_dims(test_image, axis=0)
     predictions = model.predict(test_image)
     scores = tf.nn.argmax(predictions[0])
-    scores = scores.numpy()
     print(prediction)
-    print(scores)
     return result
 
 
 if file_uploaded is not None:    
     image = Image.open(file_uploaded)
     st.image(image, caption='Uploaded Image', use_column_width=True)
-    
-
         
     if class_btn:
         if file_uploaded is None:
             st.write("Invalid command, please upload an image")
         else:
             with st.spinner('Model working....'):
-                # plt.imshow(image)
-                # plt.axis("off")
                 predictions = predictor(image)
-                st.write(predictions)
+    
                 
 
                 
