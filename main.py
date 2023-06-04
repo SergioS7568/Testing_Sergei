4b3b7357f9eee34c4411a62a8d8da0aa14248e56
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-04 18:51:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -158,6 +158,7 @@ if uploaded_file is not None:
             img_reshape = img_reshape.reshape(-1, 28, 28, 1)
             prediction =loaded_model.predict(img_reshape)
             print(prediction)
+            print(prediction.shape)
             #pred = np.exp(prediction)
             #print(pred)
             #pred = prediction.reshape(-1)
