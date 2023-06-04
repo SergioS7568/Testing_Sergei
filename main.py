21d0abe62fbebd7b68f8d274f3c8b1b99d8b65b5
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-04 17:35:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -173,6 +173,7 @@ if uploaded_file is not None:
             
             #print(prediction)
             print(prediction.shape)
+            print(classes(prediction[0]))
             #pred_class = classes[pred.argmax()]
             #plt.imshow(img_reshape)
             #plt.title(pred_class)
