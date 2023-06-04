fa332af71d0222890a9d980e1f0e2400105303a4
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-04 17:08:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class_btn = st.button("Classify")
 loaded_model = tf.keras.models.load_model("saved_model/mdl_wts2.hdf5")
     
 def predictor(image):
-    classifier_model = "keras.h5"
+    classifier_model = "mdl_wts2.hdf5"
     model = loaded_model(classifier_model)
     test_image = image.resize((200,200))
     test_image = preprocessing.image.img_to_array(test_image)
