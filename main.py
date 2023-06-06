3d13b7ec55dac377dde48a4d9c3dcb0324cbd22b
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-05 22:24:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -169,6 +169,7 @@ if uploaded_file is not None:
             file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
             img_normalized = cv2.normalize(file_bytes, None, 0, 1.0, cv2.NORM_MINMAX)
             img = img_normalized
+            img = cv2.resize(img, (28,28))
             #img = file_bytes
             #plt.imshow(tf.squeeze(img[0])) 
             pred = loaded_model.predict(np.expand_dims(img, axis=0))[0]
