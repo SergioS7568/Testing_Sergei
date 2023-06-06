09bb5ab94b8b17075d6b8c92c4ee97bd7b764869
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-05 23:36:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -183,7 +183,7 @@ if uploaded_file is not None:
             img = cv2.resize(img, (28,28))
             img_array = image.img_to_array(img)
             img_array = tf.expand_dims(img_array, 0) # Create a batch
-            pred = tf.nn.sigmoid(loaded_model.predict(img_array))[0]
+            pred = tf.nn.sigmoid(loaded_model.predict(img_array))
             print((-pred).argsort()[:5])
             
             #sigmoid
