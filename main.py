66d269a99095f89736e426bab8026a8632c86a0a
SergioS7568 <93202806+SergioS7568@users.noreply.github.com>
2023-06-07 23:01:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if uploaded_file is not None:
     #img = preprocess_image(img)  # Modify this function based on your preprocessing requirements
     def preprocess_img(img):
         #COLOR_RGB2GRAY
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         normalized = gray /255.0
         reshaped = normalized.reshape(28,28,1).astype('float32')
         
